d34c4aea6280fb8a86318d22b5e8a75923ec9f67
rkdals03 <65216898+rkdals03@users.noreply.github.com>
2023-07-12 14:26:46 +0900

    Update control_tower.py

--- examples/demos/swarm_demo/control_tower/control_tower.py
+++ examples/demos/swarm_demo/control_tower/control_tower.py
@@ -582,15 +582,16 @@ class SyncTower(TowerBase):
 
 
 uris = [
-    'radio://0/10/2M/E7E7E7E701',
-    'radio://0/10/2M/E7E7E7E702',
-    'radio://0/10/2M/E7E7E7E703',
-    'radio://0/10/2M/E7E7E7E704',
-    'radio://0/10/2M/E7E7E7E705',
-    'radio://0/10/2M/E7E7E7E706',
-    'radio://0/10/2M/E7E7E7E707',
-    'radio://0/10/2M/E7E7E7E708',
-    'radio://0/10/2M/E7E7E7E709'
+    'radio://0/80/2M/E7E7E7E701',
+    'radio://0/80/2M/E7E7E7E702',
+    'radio://0/80/2M/E7E7E7E703',
+    'radio://0/80/2M/E7E7E7E704',
+    'radio://0/80/2M/E7E7E7E705',
+    'radio://0/80/2M/E7E7E7E70A',
+    'radio://0/80/2M/E7E7E7E70B',
+    'radio://0/80/2M/E7E7E7E70C',
+    'radio://0/80/2M/E7E7E7E70D',
+    'radio://0/80/2M/E7E7E7E70E'
 ]
 
 count = 1
